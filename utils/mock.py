a132e2e1bb51dbd9005926dca77a8aa51d02d329
AbsoluteVirtue <3hitemouse@gmail.com>
2022-07-04 14:54:24 +0300

    fixed mock results

--- utils/mock.py
+++ utils/mock.py
@@ -1,36 +1,27 @@
 import random
-from types import SimpleNamespace
 
 from . import strrand
-
-
-_server = {
-    "info": {
-        "values": {
-            "server_name": "",
-            "map": "",
-            "player_count": 0,
-            "max_players": 0,
-            "vac_enabled": True,
-            "password_protected": True,
-        }
-    },
-    "players": [],
-}
 
 
 def _gen_server_list():
     for _ in range(1, 100):
         k = random.random()
-        _server["info"]["values"]["server_name"] = strrand(18)
-        _server["info"]["values"]["map"] = strrand(6)
-        _server["info"]["values"]["player_count"] = int(k * 100)
-        _server["info"]["values"]["max_players"] = 16
-        _server["info"]["values"]["vac_enabled"] = k > 0.5 or False
-        _server["info"]["values"]["password_protected"] = k > 0.5 or True
-        __players = []
+        _server = {
+            "info": {
+                "values": {
+                    "server_name": strrand(18),
+                    "map": strrand(6),
+                    "player_count": int(k * 100),
+                    "max_players": 99,
+                    "vac_enabled": k > 0.2,
+                    "password_protected": k < 0.5,
+                }
+            },
+            "players": [],
+        }
+
         for j in range(0, int(k * 100)):
-            __players.append({
+            _server["players"].append({
                 "values": {
                     "index": str(j),
                     "name": strrand(8),
@@ -39,8 +30,7 @@ def _gen_server_list():
                 },
             })
 
-        _server["players"] = __players
-        yield SimpleNamespace(**_server)
+        yield _server
 
 
 async def get_server_list_with_players(logger):
@@ -48,16 +38,16 @@ async def get_server_list_with_players(logger):
     count = 0
 
     for server in _gen_server_list():
-        s = server.info.values
+        s = server["info"]["values"]
 
         count += s['player_count']
         p = []
-        for player in server.players:
+        for player in server.get("players", []):
             p.append({
-                'index': player.values.get('index'),
-                'name': player.values['name'],
-                'score': player.values.get('score'),
-                'duration': player.values.get('duration')
+                'index': player["values"].get('index'),
+                'name': player["values"]['name'],
+                'score': player["values"].get('score'),
+                'duration': player["values"].get('duration')
             })
 
         s.update(players=p)
